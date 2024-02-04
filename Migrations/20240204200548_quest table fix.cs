6dc85ace20e030ec17acf4a45f9d31f876449453
Zachary Pruessner <zachpruessner@gmail.com>
2024-02-04 14:06:06 -0600

    quest model fix

---
+++ Migrations/20240204200548_quest table fix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskRpgApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class questtablefix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Quests",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Quests",
+                newName: "Name");
+        }
+    }
+}
